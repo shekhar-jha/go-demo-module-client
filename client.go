ff84b61398ef549d5003ad721df1a29d19dc48fb
Shekhar Jha <shekhar-jha@users.noreply.github.com>
2022-01-05 05:47:20 -0600

    V2.0Beta-With-Mod1+2

--- client.go
+++ client.go
@@ -3,7 +3,7 @@ package main
 import (
 	"github.com/shekhar-jha/go-demo-module/mod1"
 	"github.com/shekhar-jha/go-demo-module/mod2"
-	"github.com/shekhar-jha/go-demo-module/mod3"
+//	"github.com/shekhar-jha/go-demo-module/mod3"
 	"fmt"
 )
 
@@ -11,6 +11,6 @@ func main() {
 	fmt.Println("Entering main...")
 	fmt.Println("Calling mod1.Mod1(). Result:", mod1.Mod1())
 	fmt.Println("Calling mod2.Mod2(). Result:", mod2.Mod2())
-	fmt.Println("Calling mod3.Mod3(). Result:", mod3.Mod3())
+//	fmt.Println("Calling mod3.Mod3(). Result:", mod3.Mod3())
 	fmt.Println("Exiting main")
 }
